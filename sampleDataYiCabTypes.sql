776977eff84a10c178beabcf2aa2929a077d28cb
yiseoho95 <yiseoho95@gmail.com>
2021-04-24 10:39:14 -0400

    added sql sample

---
+++ sampleDataYiCabTypes.sql
@@ -0,0 +1,60 @@
+SET IDENTITY_INSERT [dbo].[BookingsHistory] ON 
+
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values(2, 'Example@Example.com','2021-01-09', '', 1, 2, 'something just return', 'Arrrgh', '2021-01-02', '', 4, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values(3, 'Example@Example.com','2021-01-09', '', 4, 2, 'something just return', 'go away', '2021-01-02', '', 4, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values(4, 'Example@Example.com','2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 4, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values(5, 'Example@Example.com','2021-01-09', '', 2, 4, 'something just return', 'go away', '2021-01-02', '', 4, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 6, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 7, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 8, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 9, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values(	10, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 11, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 2, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 12, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 3, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 13, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 2, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 14, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 2, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 15, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 2, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 16, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 3, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 17, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 3, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 18, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 3, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 19, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 1, '808-999-6726', 'returned', '', 435, '  ');
+insert into BookingsHistory(Id,Email,BookingDate,BookingTime,FromPlace,ToPlace,PickupAddress,Landmark,PickupDate,PickupTime,CabTypeId,ContactNo,Status,Comp_Time,Charge,Feedback)
+Values( 20, 'Example@Example.com', '2021-01-09', '', 3, 2, 'something just return', 'go away', '2021-01-02', '', 4, '808-999-6726', 'returned', '', 435, '  ');
+SET IDENTITY_INSERT [dbo].[BookingsHistory] OFF
+
+SET IDENTITY_INSERT [dbo].[CabTypes] ON
+INSERT INTO CabTypes(Id, Name)Values(1, 'BMW X5');
+INSERT INTO CabTypes(Id, Name)Values(2, 'GMC Denali');
+INSERT INTO CabTypes(Id, Name)Values(3, 'Mercedes-Benz GLS-AMG');
+INSERT INTO CabTypes(Id, Name)Values(4, 'Porsche GT3');
+INSERT INTO CabTypes(Id, Name)Values(5, 'Pontiac Firebird');
+SET IDENTITY_INSERT [dbo].[CabTypes] OFF
+
+SET IDENTITY_INSERT [dbo].[Places] ON
+INSERT INTO Places(Id, Name)Values(1, 'Alaska');
+INSERT INTO Places(Id, Name)Values(2, 'YellowKnife');
+INSERT INTO Places(Id, Name)Values(3, 'Vancouver');
+INSERT INTO Places(Id, Name)Values(4, 'Seattle');
+SET IDENTITY_INSERT [dbo].[Places] OFF
+
+SET IDENTITY_INSERT [dbo].[Bookings] ON
+INSERT INTO Bookings() Values();
+SET IDENTITY_INSERT [dbo].[Bookings] OFF
